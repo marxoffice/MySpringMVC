e99508db63574d42f0b4071e4d714c3bba5af984
jesson <1421280853@qq.com>
2020-11-28 22:33:37 +0800

    annotations: Autowired, Bean, Configuration, Component, ComponentScan

---
+++ src/main/java/annotation/Autowired.java
@@ -0,0 +1,14 @@
+package annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 自动装配注解
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Autowired {
+}
